5bcdde2c3f17bf1cc1a2718f31bc5e8b8ef06752
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-04-21 15:03:05 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all possible combinations of 
+ * two-digit numbers with range 0-99
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	int num1;
+	int num2;
+
+	int num0;
+	int num11;
+	int num21;
+
+	while (num <= 98)
+	{
+		num1 = (num / 10 + '0');
+		num2 = (num % 10 + '0');
+		num0 = 0;
+
+		while (num0 <= 99)
+		{
+			num11 = (num0 / 10 + '0');
+			num21 = (num0 % 10 + '0');
+
+			if (num < num0)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(' ');
+				putchar(num11);
+				putchar(num21);
+
+				if (num != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num0++;
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
