e94a4024428e1dfe54d9ff2410f79ee720b88ca2
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-04-27 16:21:54 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,18 +12,17 @@ int _putchar(char c);
 
 void print_number(int n)
 {
-	unsigned int nn = 0;
 	if (n < 0)
 	{
-		nn = -n;
+		n = -n;
 		_putchar('-');
 	}
 	if (n == 0)
 		_putchar('0');
 
 	if (n / 10)
-		print_number(nn / 10);
+		print_number(n / 10);
 
-	_putchar((nn % 10) + '0');
+	_putchar((n % 10) + '0');
 }
 
