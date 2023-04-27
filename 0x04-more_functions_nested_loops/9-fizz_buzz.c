185f308f1ba537a95119471121f6243038b65f42
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-04-27 16:05:16 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print numbers 1 to 100
- * Return: Always 0
+ * main - check the code
+ * Return: void
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
