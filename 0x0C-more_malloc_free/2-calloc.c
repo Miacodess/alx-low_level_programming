ad42fd0e1e71ebe5ca677013a59dd4d07f0524c4
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-05-18 16:35:54 -0700

    Malloc and free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,12 +30,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 
 	p = malloc(size * nmemb);
 
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 
 	_setmem(p, 0, (nmemb * size));
 
