db085b7a2ca51770cdaf6870a9e979fcd80d9eec
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-05-22 14:28:14 -0700

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the file name
+ * main - prints the file name it was compiled from
  *
- * Return: 0 (Success)
+ * Return: 0 (Always)
  */
 
 int main(void)
