fec9381b6a32ee58fc6e9127546228483922180d
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-05-22 14:26:55 -0700

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * *main - prints the file name
+ * main - prints the file name
  *
- * *Return: 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	printf("The file name is: %s\n", __FILE__);
+	
 	return (0);
 }
