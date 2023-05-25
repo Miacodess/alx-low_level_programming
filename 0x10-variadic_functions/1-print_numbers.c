652c3c9532302ec3dc22d969e90ddd40fa34ea5b
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-05-25 13:34:43 -0700

    C- Variadic-functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -28,7 +28,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i == n - 1)
 				printf("%d\n", num);
 			else
-				printf("%d\n", num);
+				printf("%d", num);
 		}
 		else
 		{
