e891b617f37c70b27485beda34974f22f7ca48d5
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-06-06 16:12:46 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		listint_t* temp = *head;
-		*head = (*head) ->next;
+		*head = (*head)->next;
 		free(temp);
 		return (1);
 	}
