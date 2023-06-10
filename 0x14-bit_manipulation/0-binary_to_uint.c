e79a1dd9f80b33eca51df1ea7eef6054c1b4ecef
Ifeoluwa <ifeoluwaakapo3@gmail.com>
2023-06-09 18:27:48 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,13 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int res = 0;
+        int i = 0;
+
 	if (b == NULL)
 	{
 		return (0);
 	}
-
-	unsigned int res = 0;
-	int i = 0;
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
